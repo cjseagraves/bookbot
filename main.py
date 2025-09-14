6f061b1a55a466c5810d152ade9086c0d46101c5
Chris Seagraves <cjseagraves@live.com>
2025-09-13 23:29:37 -0400

    CH2 L5: Refactor

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
+from stats import get_num_words
+
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
         file_contents = f.read()
     return file_contents
-def get_num_words(file_contents):
-    num_words = len(file_contents.split())
-    return num_words
 
 def main():
     file_contents = get_book_text("books/frankenstein.txt")
---
+++ stats.py
@@ -0,0 +1,4 @@
+def get_num_words(file_contents):
+    num_words = len(file_contents.split())
+    return num_words
+
