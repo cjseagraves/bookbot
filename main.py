4e1df31e50466e6d8a34fea09e77f91926463e19
Chris Seagraves <cjseagraves@live.com>
2025-09-13 23:20:08 -0400

    CH2 L4: Count Words

--- main.py
+++ main.py
@@ -2,8 +2,14 @@ def get_book_text(path_to_file):
     with open(path_to_file) as f:
         file_contents = f.read()
     return file_contents
+def get_num_words(file_contents):
+    num_words = len(file_contents.split())
+    return num_words
 
 def main():
-    print(get_book_text("books/frankenstein.txt"))
+    file_contents = get_book_text("books/frankenstein.txt")
+    num_words= get_num_words(file_contents)
+    print(f"{num_words} words found in the document")
+    
 
 main()
